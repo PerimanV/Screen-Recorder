c18155c787ad5fed9285c84fec93df68e16fe215
Periklis Vovos <138630140+PerimanV@users.noreply.github.com>
2024-11-11 14:43:35 +0200

    Add files via upload

---
+++ ScreenRecorder/ScreenRecorder.py
@@ -0,0 +1,30 @@
+import pyautogui
+import cv2
+import numpy as np
+
+#confirgure settings of recording
+resolution = (1920, 1080)
+codec = cv2.VideoWriter_fourcc(*"XVID")
+filename = "C:/Users/perim/Videos/PythonRecordings/Recording.avi"
+fps = 20.0 #set to 20 fps because 60 fps seemed too fast in the end
+out = cv2.VideoWriter(filename, codec, fps, resolution)
+
+
+#create recording window
+cv2.namedWindow("Recording", cv2.WINDOW_NORMAL) 
+cv2.resizeWindow("Recording", 480, 270) 
+
+#start recording
+while True:
+    img = pyautogui.screenshot()
+    frame = np.array(img) #convert image to numpy array
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB) #convert to RGB
+    out.write(frame) 
+    cv2.imshow('Recording', frame) 
+
+    if cv2.waitKey(1) == ord('0'): #press '0' to stop recording
+        break
+
+#release resources
+out.release()
+cv2.destroyAllWindows() 
